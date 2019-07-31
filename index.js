0dfb95d462a8bc7fb3c1d16312b55e748e98fdeb
Sergey Bolshchikov <sergey@bolshchikov.net>
2019-07-31 14:02:40 +0300

    remove https

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const { Client, createAccount, argUint64 } = require('orbs-client-sdk');
 
 const INTERVAL = 60 * 1000;
 const VCHAIN = 1000;
-const NODE_URL = `https://node1.demonet.orbs.com/vchains/${VCHAIN}`;
+const NODE_URL = `http://node1.demonet.orbs.com/vchains/${VCHAIN}`;
 
 const { privateKey, publicKey } = createAccount();
 const orbClient = new Client(NODE_URL, VCHAIN, 'TEST_NET');
