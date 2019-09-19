fb614a28ac5ae38193a01069e6748526fd48ae6a
Sergey Bolshchikov <sergey@bolshchikov.net>
2019-09-19 14:08:19 +0300

    ping production node

--- src/prod.js
+++ src/prod.js
@@ -4,7 +4,7 @@ const INTERVAL = 30 * 1000;
 
 const PROD_VCHAIN = 1100000;
 const PROD_NODE_URL = `http://validator.orbs.com/vchains/${PROD_VCHAIN}`;
-const CONTRACT_NAME = 'BenchmarkContract';
+const CONTRACT_NAME = 'BenchmarkCounter';
 
 const { privateKey, publicKey } = createAccount();
 const orbClient = new Client(PROD_NODE_URL, PROD_VCHAIN, 'MAIN_NET');
